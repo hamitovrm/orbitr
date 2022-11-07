791ab473bae823b633ba0b05af97470c2bdcdaa2
hamitovrm <96112608+hamitovrm@users.noreply.github.com>
2022-11-07 13:13:57 +0300

    Update orbitr.py

--- orbitr.py
+++ orbitr.py
@@ -32,12 +32,13 @@ num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 def predict_step(image_paths):
   images = []
-  for image_path in image_paths:
-    i_image = Image.open(image_path)
-    if i_image.mode != "RGB":
-      i_image = i_image.convert(mode="RGB")
+  #for image_path in image_paths:
+    #i_image = Image.open(image_path)
+. i_image= image_paths
+  if i_image.mode != "RGB":
+    i_image = i_image.convert(mode="RGB")
 
-    images.append(i_image)
+  images.append(i_image)
 
   pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
   pixel_values = pixel_values.to(device)
