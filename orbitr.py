0b368c15914572271f2f22216b10cb51c08d7cec
hamitovrm <96112608+hamitovrm@users.noreply.github.com>
2022-11-07 16:30:33 +0300

    Update orbitr.py

--- orbitr.py
+++ orbitr.py
@@ -3,8 +3,9 @@ import requests
 import streamlit as st
 from PIL import Image
 import numpy as np
-#import torch
-from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer, AutoModel
+import jax
+from transformers import ViTFeatureExtractor, AutoTokenizer, FlaxVisionEncoderDecoderModel
+
 
 API_URL_ta = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-mul"
 headers = {"Authorization": f"Bearer {'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'}"}
@@ -13,40 +14,28 @@ def translate(payload, API_URL):
 	response = requests.post(API_URL, headers=headers, json=payload )
 	return response.json
 	
+loc = "ydshieh/vit-gpt2-coco-en"
 
-	
-	
+feature_extractor = ViTFeatureExtractor.from_pretrained(loc)
+tokenizer = AutoTokenizer.from_pretrained(loc)
+model = FlaxVisionEncoderDecoderModel.from_pretrained(loc)
 
-feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-model = AutoModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-
-
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#model.to(device)
+gen_kwargs = {"max_length": 16, "num_beams": 4}
 
 
-max_length = 16
-num_beams = 4
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-def predict_step(image_paths):
-  images = []
-  #for image_path in image_paths:
-    #i_image = Image.open(image_path)
-  i_image= image_paths
-  if i_image.mode != "RGB":
-    i_image = i_image.convert(mode="RGB")
+# This takes sometime when compiling the first time, but the subsequent inference will be much faster
 
-  images.append(i_image)
-
-  pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-  pixel_values = pixel_values.to(device)
-
-  output_ids = model.generate(pixel_values, **gen_kwargs)
-
-  preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-  preds = [pred.strip() for pred in preds]
-  return preds
+def generate(pixel_values):
+    output_ids = model.generate(pixel_values, **gen_kwargs).sequences
+    return output_ids
+    
+    
+def predict(image):
+    pixel_values = feature_extractor(images=image, return_tensors="np").pixel_values
+    output_ids = generate(pixel_values)
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds] 
+    return preds
 
 
 def load_image():
@@ -58,7 +47,6 @@ def load_image():
     else:
         return None
 
-
 def print_predictions(preds):
     classes = decode_predictions(preds, top=3)[0]
     for cl in classes:
@@ -69,12 +57,11 @@ def print_predictions(preds):
         for tt in tr_test:
             st.write(str(tt['translation_text']))
 
-
 st.title('Распознавание объектов с переводом на разные языки')
 img = load_image()
 result = st.button('Распознать изображение')
 if result:
-    preds = predict_step(img)
+    preds = predict(img)
     st.write('**Результаты распознавания:**')
     print_predictions(preds)
 
