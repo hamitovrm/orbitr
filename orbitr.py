f4ea6fafd8c80096014cb977660c4efa28374165
hamitovrm <96112608+hamitovrm@users.noreply.github.com>
2022-11-08 17:36:21 +0300

    Update orbitr.py

--- orbitr.py
+++ orbitr.py
@@ -11,9 +11,6 @@ tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-
-
-
 max_length = 16
 num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
@@ -23,17 +20,12 @@ def predict_step(image):
   if image.mode != "RGB":
      image = image.convert(mode="RGB")
   images.append(image)
-
   pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
   pixel_values = pixel_values.to(device)
-
   output_ids = model.generate(pixel_values, **gen_kwargs)
-
   preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
   preds = [pred.strip() for pred in preds]
   return preds
-
-
 
 
 API_URL_ta = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-mul"
@@ -42,7 +34,6 @@ headers = {"Authorization": f"Bearer {'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'}"}
 def translate(payload, API_URL):
 	response = requests.post(API_URL, headers=headers, json=payload )
 	return response.json
-	
 
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
@@ -70,14 +61,14 @@ with Image.open(requests.get(url, stream=True).raw) as image:
     print_predictions(preds)        
     st.write(type(image))    
             
-st.title('Распознавание объектов с переводом на разные языки')
-image_data=load_image()
-st.write(type(image_data))
-result = st.button('Распознать изображение')
-if result:
-    with image_data as image:
-        st.image(image)
-        preds = predict_step(image)
-        st.write('**Результаты распознавания:**')
-        st.write(str(preds))
+#st.title('Распознавание объектов с переводом на разные языки')
+#image_data=load_image()
+#st.write(type(image_data))
+#result = st.button('Распознать изображение')
+#if result:
+#    with image_data as image:
+#        st.image(image)
+#        preds = predict_step(image)
+#        st.write('**Результаты распознавания:**')
+#       st.write(str(preds))
 
