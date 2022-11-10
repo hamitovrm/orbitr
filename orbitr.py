38e2c6dfd011a217a1ce6052c327e21877139442
hamitovrm <96112608+hamitovrm@users.noreply.github.com>
2022-11-10 16:54:07 +0300

    Update orbitr.py

--- orbitr.py
+++ orbitr.py
@@ -39,8 +39,8 @@ def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
-        st.image(image_data)
-        st.write(type(image_data))
+        #st.image(image_data)
+        #st.write(type(image_data))
         return Image.open(io.BytesIO(image_data))
     else:
         return None
