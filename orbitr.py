825b303f2659d4e812a4c7626dd0a29bc2199554
hamitovrm <96112608+hamitovrm@users.noreply.github.com>
2022-11-21 10:48:23 +0300

    Update orbitr.py

--- orbitr.py
+++ orbitr.py
@@ -8,15 +8,12 @@ from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTok
 @st.cache(allow_output_mutation=True)
 def load_model():
     return VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-
-feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-max_length = 16
-num_beams = 4
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+@st.cache(allow_output_mutation=True)
+def load_feature_extractor():
+    return ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+@st.cache(allow_output_mutation=True)
+def load_tokenizer():
+    return AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
 def predict_step(image):
   images = []
@@ -29,10 +26,6 @@ def predict_step(image):
   preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
   preds = [pred.strip() for pred in preds]
   return preds
-
-
-API_URL_ta = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-mul"
-headers = {"Authorization": f"Bearer {'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'}"}
 
 def translate(payload, API_URL):
 	response = requests.post(API_URL, headers=headers, json=payload )
@@ -56,7 +49,20 @@ def print_predictions(preds):
         for tt in tr_test:
             st.write(str(tt['translation_text']))
 
+	
+st.title('Распознавание объектов с переводом на разные языки')	
 model = load_model()	
+feature_extractor = load_feature_extractor()
+tokenizer=load_tokenizer()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+API_URL_ta = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-mul"
+headers = {"Authorization": f"Bearer {'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'}"}
+
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 with Image.open(requests.get(url, stream=True).raw) as image1:
     st.image(image1)
@@ -65,7 +71,6 @@ with Image.open(requests.get(url, stream=True).raw) as image1:
     print_predictions(preds)        
     st.write(type(image1))    
             
-st.title('Распознавание объектов с переводом на разные языки')
 im=load_image()
 result = st.button('Распознать изображение')
 if result:
