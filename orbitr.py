ddc0704aad0c47dd311a28534d5f49da666369e1
hamitov renat <hamitov@live.ru>
2022-12-03 18:54:52 +0300

    modified:   orbitr.py

--- orbitr.py
+++ orbitr.py
@@ -5,15 +5,15 @@ import streamlit as st
 from PIL import Image
 from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
 
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-max_length = 16
-num_beams = 4
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+@st.cache(allow_output_mutation=True)
+def load_feature_extractor():
+    return ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+@st.cache(allow_output_mutation=True)
+def load_tokenizer():
+    return AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
 def predict_step(image):
   images = []
@@ -27,16 +27,12 @@ def predict_step(image):
   preds = [pred.strip() for pred in preds]
   return preds
 
-
-API_URL_ta = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-mul"
-headers = {"Authorization": f"Bearer {'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'}"}
-
 def translate(payload, API_URL):
 	response = requests.post(API_URL, headers=headers, json=payload )
 	return response.json
 
 def load_image():
-    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    uploaded_file = st.file_uploader(label='Загрузите изображение:')
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -48,25 +44,47 @@ def print_predictions(preds):
     for cl in preds:
         #st.write(str(cl).replace('_'," "))
         en_text=str(cl).replace('_'," ")
-        trans_ta = translate({"inputs": [">>rus<< "+en_text, ">>tat<< "+en_text, ">>deu<< "+en_text,],}, API_URL_ta)
+        trans_ta = translate({"inputs": [">>rus<< "+en_text, ">>tat<< "+en_text, ">>deu<< "+en_text, ">>fra<< "+en_text, ">>ita<< "+en_text,]}, API_URL_ta)
+        sleep_duration = 0.5
         tr_test=tuple(trans_ta())
-        for tt in tr_test:
-            st.write(str(tt['translation_text']))
-            
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-with Image.open(requests.get(url, stream=True).raw) as image1:
-    st.image(image1)
-    preds = predict_step(image1)
-    st.write(preds)
-    print_predictions(preds)        
-    st.write(type(image1))    
-            
+        sleep_duration = 0.5
+        st.write('рус: ', tr_test[0]["translation_text"])
+        st.write('тат: ', tr_test[1]["translation_text"])
+        st.write('deu: ', tr_test[2]["translation_text"])
+        st.write('fra:', tr_test[3]["translation_text"])
+        st.write('ita:', tr_test[4]["translation_text"])
+        #for tt in tr_test:
+        #    st.write(str(tt['translation_text']))
+	    
+
 st.title('Распознавание объектов с переводом на разные языки')
+model = load_model()	
+feature_extractor = load_feature_extractor()
+tokenizer=load_tokenizer()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+API_URL_ta = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-mul"
+headers = {"Authorization": f"Bearer {'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'}"}
+
+#Тестовое изображение
+#url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+#with Image.open(requests.get(url, stream=True).raw) as image1:
+#    st.image(image1)
+#    preds = predict_step(image1)
+#    st.write(preds)
+#    print_predictions(preds)        
+            
 im=load_image()
-result = st.button('Распознать изображение')
+sleep_duration = 0.5
+result = st.button('Распознать и перевести:')
 if result:
-   st.write(type(im))
    preds = predict_step(im)
-   st.write('**Результаты распознавания:**')
+   st.write('**На картинке:**')
    st.write(str(preds))
+   sleep_duration = 0.5
+   print_predictions(preds) 
 
